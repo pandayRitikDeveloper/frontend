845a99c653a337ebee87695ca733f1c6be313f17
Ritik Panday <panday.ritik@tftus.com>
2023-07-20 13:15:04 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Mobulous-Machine-Test-MEAN-In_angular';
+}
